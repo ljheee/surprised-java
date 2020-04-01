2eb82082c18886838bf53c9457f9b26cdd2c97cf
lijianhua04 <lijianhua04@meituan.com>
2020-04-01 16:17:58 +0800

    类加载-阶段

---
+++ src/com/ljheee/surprised/classloading/ClassLoadingStep.java
@@ -0,0 +1,35 @@
+package com.ljheee.surprised.classloading;
+
+/**
+ * main方法中首次使用单例，进行类加载（装载、链接、初始化）；
+ * 链接-准备阶段，类字段赋初始值；
+ * 初始化阶段，类字段赋正确的值；
+ * <p>
+ * 初始化时singleton先调用构造器（numB++），而后又给numB赋值0；
+ */
+public class ClassLoadingStep {
+
+    public static void main(String[] args) {
+        System.out.println("通过单例访问numA=" + Singleton.numA);
+        System.out.println("通过单例访问numB=" + Singleton.numB);
+    }
+}
+
+class Singleton {
+    public static int numA = 0;
+
+    private static Singleton singleton = new Singleton();
+
+    public Singleton() {
+        numA++;
+        numB++;
+        System.out.println("numA=" + numA);
+        System.out.println("numB=" + numB);
+    }
+
+    public static int numB = 5;
+
+    public static Singleton getInstance() {
+        return singleton;
+    }
+}
